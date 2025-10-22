Train cifar cnn with correct mean/std broadcasting/calc

models/cifar_cnn.py
@@ -9,8 +9,8 @@ class Normalize(nn.Module):
 
     def __init__(self, mean: torch.Tensor, std: torch.Tensor):
         super().__init__()
-        self.register_buffer("mean", mean)
-        self.register_buffer("std", std)
+        self.register_buffer("mean", mean.reshape(1, -1, 1, 1))
+        self.register_buffer("std", std.reshape(1, -1, 1, 1))
 
     def forward(self, x):
         return (x - self.mean) / self.std
@@ -19,10 +19,8 @@ class Normalize(nn.Module):
 class CifarConvNet(nn.Module):
     def __init__(
         self,
-        mean: torch.Tensor = torch.tensor([0.4914, 0.4822, 0.4465]).reshape(
-            1, -1, 1, 1
-        ),
-        std: torch.Tensor = torch.tensor([0.2470, 0.2435, 0.2616]).reshape(1, -1, 1, 1),
+        mean: torch.Tensor = torch.tensor([0.4914, 0.4822, 0.4465]),
+        std: torch.Tensor = torch.tensor([0.2470, 0.2435, 0.2616]),
     ):
         super().__init__()
         self.net = nn.Sequential(